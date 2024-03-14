a07eb65a60ca31cb767ecbe0051d60778b6c7065
Merbit <138371967+merbit@users.noreply.github.com>
2024-03-14 20:42:06 +0800

    Create WBBlocks.java

---
+++ src/wayback/content/WBBlocks.java
@@ -0,0 +1,23 @@
+package wayback.content;
+
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.turret.*;
+
+import static mindustry.type.ItemStack.with;
+import static mindustry.content.Items.*;
+
+public class WBBlocks {
+  public static Block
+  // turret
+  binary;
+
+  // bullet objects for turrets will be made on a separate file
+  // to make balancing easier. it'll be harder if everything was inlined here.
+  public static void load(){
+    binary = new ItemTurret("binary"){{
+      // temporary requirements, will balance later
+      requirements(Category.turret, with(copper, 35, lead, 25));
+    }};
+  }
+}
